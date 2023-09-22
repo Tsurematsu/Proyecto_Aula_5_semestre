3d27cd5ee395702301f13868c59ce5a693b90742
José Daniel <daniel120802@gmail.com>
2023-09-22 10:59:58 -0700

    create

---
+++ backend/src/test/java/back/appi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package back.appi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
