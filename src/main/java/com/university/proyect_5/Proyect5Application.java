4694cc81b3c1b6ded61316b58f0aebc1c5ad0201
José Daniel <daniel120802@gmail.com>
2023-09-22 09:54:15 -0700

    create project

---
+++ src/main/java/com/university/proyect_5/Proyect5Application.java
@@ -0,0 +1,13 @@
+package com.university.proyect_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/proyect_5/Proyect5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.proyect_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
